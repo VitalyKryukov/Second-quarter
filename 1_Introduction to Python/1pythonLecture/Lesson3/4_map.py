3f1ffc764fe51203ad31914a650c309cc01abf61
Vitaly Kryukov <v.b.kryukov@mail.ru>
2023-01-15 22:42:09 +0300

    Добавил условия к ДЗ 5 семинара

--- 1_Introduction to Python/1pythonLecture/Lesson3/4_map.py
+++ 1_Introduction to Python/1pythonLecture/Lesson3/4_map.py
@@ -1,20 +1,20 @@
-# 1 Пример
+# # 1 Пример
 # li = [x for x in range(1,20)]
 
 # li = list(map(lambda x: x+10, li))
 
 # print(li)
 
-# 2 Пример
+# # 2 Пример
 # data = list(map(int, input().split()))
 # print(data)
 
-# 3 Пример
+# # 3 Пример
 # data = map(int, input().split())
 # for e in data:
 #     print(e)
 
-# 4 Пример - без сохранения в list можно пробежаться только 1 раз
+# # 4 Пример - без сохранения в list можно пробежаться только 1 раз
 # data = map(int,'1 2 3'.split())
 
 # for e in data:
