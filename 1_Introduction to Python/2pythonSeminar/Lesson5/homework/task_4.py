2a9d2d01c7d7159262773f8c2925c3b792e2c1d7
Vitaly Kryukov <v.b.kryukov@mail.ru>
2023-01-17 23:49:03 +0300

    Семинар 6

--- 1_Introduction to Python/2pythonSeminar/Lesson5/homework/task_4.py
+++ 1_Introduction to Python/2pythonSeminar/Lesson5/homework/task_4.py
@@ -1,4 +1,50 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
 
+def rle_encode(data):
+    encoding = ''
+    prev_char = ''
+    count = 1
 
+    if not data: return ''
+
+    for char in data:
+        # If the prev and current characters
+        # don't match...
+        if char != prev_char:
+            # ...then add the count and character
+            # to our encoding
+            if prev_char:
+                encoding += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            # Or increment our counter
+            # if the characters do match
+            count += 1
+    else:
+        # Finish off the encoding
+        encoding += str(count) + prev_char
+        return encoding
+
+def rle_decode(data):
+    decode = ''
+    count = ''
+    for char in data:
+        # If the character is numerical...
+        if char.isdigit():
+            # ...append it to our count
+            count += char
+        else:
+            # Otherwise we've seen a non-numerical
+            # character and need to expand it for
+            # the decoding
+            decode += char * int(count)
+            count = ''
+    return decode
+
+encoded_val = rle_encode('AAAAAAFDDCCCCCCCAEEEEEEEEEEEEEEEEE')
+print(encoded_val)
+
+decoded_val = rle_decode('6A1F2D7C1A17E')
+print(decoded_val)
