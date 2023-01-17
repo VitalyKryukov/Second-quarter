cd041bf14b52c05640df6b0b57485341f69fdd63
Vitaly Kryukov <v.b.kryukov@mail.ru>
2023-01-17 23:49:39 +0300

    Семинар 6

---
+++ 1_Introduction to Python/2pythonSeminar/Lesson6/seminar/task_1.py
@@ -0,0 +1,120 @@
+# 1. Напишите программу вычисления арифметического выражения заданного строкой.
+# Используйте операции +,-,/,*. приоритет операций стандартный. 
+    
+#     *Пример:*  
+#     2+2 => 4;   
+#     1+2*3 => 7;  
+#     1-2*3 => -5; 
+#     - Добавьте возможность использования скобок, меняющих приоритет операций.
+        
+#     *Пример:*   
+#     1+2*3 => 7;   
+#     (1+2)*3 => 9;
+
+# ВАЖНО!!!
+## print(eval(s))# никогда не пользоваться!!!!!!!! Может форматнуть диск!!!!!!!
+
+# # Вариант 1
+
+# s = '3*(10+12)'
+# num = ''
+# my_list = []
+# for elem in s:
+#     if elem.isdigit():
+#         num += elem
+#     elif elem in '()':
+#        my_list.append(elem) 
+#     else:
+#         my_list.append(int(num))
+#         my_list.append(elem)
+#         num = ''
+# if s[-1] == ')':
+#     my_list.insert(-1, int(num))
+# else:
+#     my_list.append(int(num))
+# print(my_list)
+# exit()
+
+# if '(' in my_list:
+#     first_i = my_list.index('(')
+
+# if '*' in my_list or '*' in my_list:
+#     for i in range(1, len(my_list), 2):
+#         if my_list[i] == '*':
+#             result = my_list.pop(i+1) * my_list.pop(i-1)
+#             my_list[i-1] = result
+#         elif my_list[i] == '/':
+#             result = my_list.pop(i+1) / my_list.pop(i-1)
+#             my_list[i-1] = result
+
+# if '+' in my_list or '-' in my_list: 
+#     for i in range(1, len(my_list), 2):
+#         if my_list[i] == '-':
+#             result = my_list.pop(i+1) - my_list.pop(i-1)
+#             my_list[i-1] = result
+#         elif my_list[i] == '+':
+#             result = my_list.pop(i+1) + my_list.pop(i-1)
+#             my_list[i-1] = result
+
+# print(my_list)
+
+#Вариант 2
+OPERATORS = {'+': (1, lambda x, y: x + y), '-': (1, lambda x, y: x - y),
+             '*': (2, lambda x, y: x * y), '/': (2, lambda x, y: x / y)}
+
+def parse(formula_string):#Парсер исходной строки
+        number = ''
+        for s in formula_string:
+            if s in '1234567890.': # если символ - цифра, то собираем число
+                number += s  
+            elif number: # если символ не цифра, то выдаём собранное число и начинаем собирать заново
+                yield float(number) 
+                number = ''
+            if s in OPERATORS or s in "()": # если символ - оператор или скобка, то выдаём как есть
+                yield s 
+        if number:  # если в конце строки есть число, выдаём его
+            yield float(number)
+
+def shunting_yard(parsed_formula):#Алгоритм сортировочной станции
+        stack = []  # в качестве стэка используем список
+        for token in parsed_formula:
+            # если элемент - оператор, то отправляем дальше все операторы из стека, 
+            # чей приоритет больше или равен пришедшему,
+            # до открывающей скобки или опустошения стека.
+            # здесь мы пользуемся тем, что все операторы право-ассоциативны
+            if token in OPERATORS: 
+                while stack and stack[-1] != "(" and OPERATORS[token][0] <= OPERATORS[stack[-1]][0]:
+                    yield stack.pop()
+                stack.append(token)
+            elif token == ")":
+                # если элемент - закрывающая скобка, выдаём все элементы из стека, до открывающей скобки,
+                # а открывающую скобку выкидываем из стека.
+                while stack:
+                    x = stack.pop()
+                    if x == "(":
+                        break
+                    yield x
+            elif token == "(":
+                # если элемент - открывающая скобка, просто положим её в стек
+                stack.append(token)
+            else:
+                # если элемент - число, отправим его сразу на выход
+                yield token
+        while stack:
+            yield stack.pop()
+
+def calc(polish):# Вычислитель
+        stack = []
+        for token in polish:
+            if token in OPERATORS:  # если приходящий элемент - оператор,
+                y, x = stack.pop(), stack.pop()  # забираем 2 числа из стека
+                stack.append(OPERATORS[token][1](x, y)) # вычисляем оператор, возвращаем в стек
+            else:
+                stack.append(token)
+        return stack[0] # результат вычисления - единственный элемент в стеке
+
+def eval_(formula_string):
+    parse()
+    
+
+        
